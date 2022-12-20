03ce595eabd114c0a41a9db599181390f2863080
davovko <117132994+davovko@users.noreply.github.com>
2022-12-20 14:54:37 -0500

    commit antes de crear la clase UserDetailsImplementation

---
+++ Backend/src/main/java/com/sophos/sophosBank/security/UserDetailServiceImplementation.java
@@ -0,0 +1,25 @@
+package com.sophos.sophosBank.security;
+
+import com.sophos.sophosBank.entity.User;
+import com.sophos.sophosBank.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImplementation implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findOneByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("El usuario " + email + " no existe"));
+
+        return null;
+    }
+}
