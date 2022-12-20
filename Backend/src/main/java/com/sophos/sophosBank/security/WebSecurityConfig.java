d08361ee65c660220021e1e38bd43c75d90f0040
davovko <117132994+davovko@users.noreply.github.com>
2022-12-20 14:18:44 -0500

    commit antes de crear la clase AuthCredentials

--- Backend/src/main/java/com/sophos/sophosBank/security/WebSecurityConfig.java
+++ Backend/src/main/java/com/sophos/sophosBank/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.sophos.sophosBank.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
@@ -43,8 +44,16 @@ public class WebSecurityConfig {
     }
 
     @Bean
+    AuthenticationManager authenticationManager(HttpSecurity http ) throws Exception{
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
+    }
+
+    @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
-
     }
 }
