14a142d28d8ba96401fdb7f7c99b6ed7b886032e
davovko <117132994+davovko@users.noreply.github.com>
2022-12-21 18:31:18 -0500

    commit agregar filtro a JWT

--- Backend/src/main/java/com/sophos/sophosBank/security/JWTAuthorizationFilter.java
+++ Backend/src/main/java/com/sophos/sophosBank/security/JWTAuthorizationFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -25,6 +26,19 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
             UsernamePasswordAuthenticationToken usernamePAT = TokenUtils.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(usernamePAT);
         }
+
         filterChain.doFilter(request, response);
+
     }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request)
+            throws ServletException {
+        String path = request.getRequestURI();
+        System.out.println(path);
+        return "/v0/api/customers".equals(path);
+    }
+
+
+
 }
